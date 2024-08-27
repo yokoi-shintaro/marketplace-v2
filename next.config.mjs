7c7f8a0aa96cbfe2d00ddc779feb2617c947c447
yokoi-shintaro <32443626+yokoi-shintaro@users.noreply.github.com>
2024-08-27 11:39:52 +0900

    ソース修正

--- next.config.mjs
+++ next.config.mjs
@@ -65,8 +65,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // すべてのAPIルートにマッチ
-        source: "/api/:path*",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
